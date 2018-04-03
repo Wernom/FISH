ebca917b0e4ad2498e631ecb4fe636e9af83e8e7
wernom <loic.sagona@gmail.com>
2018-04-03 17:19:31 +0200

    Initialisation

---
+++ fish.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "cmdline.h"
+
+#define BUFLEN 2048
+
+#define YESNO(i) ((i) ? "Y" : "N")
+
+int main() {
+  struct line li;
+  char buf[BUFLEN];
+
+  line_init(&li);
+
+  for (;;) {
+    printf("fish> ");
+    fgets(buf, BUFLEN, stdin);
+
+    int err = line_parse(&li, buf);
+    if (err) { 
+      //the command line entered by the user isn't valid
+      line_reset(&li);
+      continue;
+    }
+
+    fprintf(stderr, "Command line:\n");
+    fprintf(stderr, "\tNumber of commands: %zu\n", li.ncmds);
+
+    for (size_t i = 0; i < li.ncmds; ++i) {
+      fprintf(stderr, "\t\tCommand #%zu:\n", i);
+      fprintf(stderr, "\t\t\tNumber of args: %zu\n", li.cmds[i].nargs);
+      fprintf(stderr, "\t\t\tArgs:");
+      for (size_t j = 0; j < li.cmds[i].nargs; ++j) {
+        fprintf(stderr, " \"%s\"", li.cmds[i].args[j]);
+      }
+      fprintf(stderr, "\n");
+    }
+
+    fprintf(stderr, "\tRedirection of input: %s\n", YESNO(li.redirect_input));
+    if (li.redirect_input) {
+      fprintf(stderr, "\t\tFilename: '%s'\n", li.file_input);
+    }
+
+    fprintf(stderr, "\tRedirection of output: %s\n", YESNO(li.redirect_output));
+    if (li.redirect_output) {
+      fprintf(stderr, "\t\tFilename: '%s'\n", li.file_output);
+    }
+
+    fprintf(stderr, "\tBackground: %s\n", YESNO(li.background));
+
+    /* do something with li */
+
+    line_reset(&li);
+  }
+  
+  return 0;
+}
+
